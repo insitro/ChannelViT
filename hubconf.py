ca82b7cdcc6ebcc9d20cc440bbd827f36d096469
Srinivasan Sivanandan <srinivasan@insitro.com>
2024-02-21 22:50:58 +0000

    add hubconf for pretrained model weights

--- hubconf.py
+++ hubconf.py
@@ -5,16 +5,112 @@ from channelvit.backbone.channel_vit import channelvit_small
 
 def imagenet_channelvit_small_p16_DINO(pretrained=True, *args, **kwargs):
     """
+    Pretrained ChannelViT-Small model (patch size = 16) trained on ImageNet using DINO
     """
-    # Call the model, load pretrained weights
     model = channelvit_small(patch_size=16, in_chans=3, *args, **kwargs)
     model.load_state_dict(
         hub.load_state_dict_from_url(
-            "https://data.aws.insitro.com/account/insitro-root/s3/insitro-user/srinivasan/checkpoints/channelvit_small_imagenet_bs_256_fp32/epoch=99-step=500400.ckpt",
+            "https://github.com/insitro/ChannelViT/releases/download/v1.0.0/imagenet_channelvit_small_p16_DINO.pth",
             progress=True
         )
     )
     # Set the model to evaluation mode
     model.eval()
+    return model
 
+def imagenet_channelvit_small_p16_with_hcs_supervised(pretrained=True, *args, **kwargs):
+    """
+    Pretrained Supervised ChannelViT-Small model (patch size = 16) trained on ImageNet
+    """
+    model = channelvit_small(patch_size=16, in_chans=3, *args, **kwargs)
+    model.load_state_dict(
+        hub.load_state_dict_from_url(
+            "https://github.com/insitro/ChannelViT/releases/download/v1.0.0/imagenet_channelvit_small_p16_with_hcs_supervised.pth",
+            progress=True
+        )
+    )
+    # Set the model to evaluation mode
+    model.eval()
+    return model
+
+
+def cpjump_cellpaint_channelvit_small_p8_with_hcs_supervised(pretrained=True, *args, **kwargs):
+    """
+    Pretrained Supervised ChannelViT-Small model (patch size = 8) trained on
+    CellPainting channels from JUMP-CP (subset)
+    """
+    model = channelvit_small(patch_size=8, in_chans=5, *args, **kwargs)
+    model.load_state_dict(
+        hub.load_state_dict_from_url(
+            "https://github.com/insitro/ChannelViT/releases/download/v1.0.0/cpjump_cellpaint_channelvit_small_p8_with_hcs_supervised.pth",
+            progress=True
+        )
+    )
+    # Set the model to evaluation mode
+    model.eval()
+    return model
+
+def cpjump_cellpaint_bf_channelvit_small_p8_with_hcs_supervised(pretrained=True, *args, **kwargs):
+    """
+    Pretrained Supervised ChannelViT-Small model (patch size = 8) trained on
+    CellPainting + Brightfield channels from JUMP-CP (subset)
+    """
+    model = channelvit_small(patch_size=8, in_chans=8, *args, **kwargs)
+    model.load_state_dict(
+        hub.load_state_dict_from_url(
+            "https://github.com/insitro/ChannelViT/releases/download/v1.0.0/cpjump_cellpaint_bf_channelvit_small_p8_with_hcs_supervised.pth",
+            progress=True
+        )
+    )
+    # Set the model to evaluation mode
+    model.eval()
+    return model
+
+def so2sat_channelvit_small_p8_with_hcs_random_split_supervised(pretrained=True, *args, **kwargs):
+    """
+    Pretrained Supervised ChannelViT-Small model (patch size = 8) trained on
+    all channels from So2Sat dataset (random split)
+    """
+    model = channelvit_small(patch_size=8, in_chans=18, *args, **kwargs)
+    model.load_state_dict(
+        hub.load_state_dict_from_url(
+            # TODO: replace with github release link
+            "https://github.com/insitro/ChannelViT/releases/download/v1.0.0/so2sat_channelvit_small_p8_with_hcs_random_split_supervised.pth",
+            progress=True
+        )
+    )
+    # Set the model to evaluation mode
+    model.eval()
+    return model
+
+def so2sat_channelvit_small_p8_with_hcs_hard_split_supervised(pretrained=True, *args, **kwargs):
+    """
+    Pretrained Supervised ChannelViT-Small model (patch size = 8) trained on
+    all channels from So2Sat dataset (hard split)
+    """
+    model = channelvit_small(patch_size=8, in_chans=18, *args, **kwargs)
+    model.load_state_dict(
+        hub.load_state_dict_from_url(
+            "https://github.com/insitro/ChannelViT/releases/download/v1.0.0/so2sat_channelvit_small_p8_with_hcs_hard_split_supervised.pth",
+            progress=True
+        )
+    )
+    # Set the model to evaluation mode
+    model.eval()
+    return model
+
+def camelyon_channelvit_small_p8_with_hcs_supervised(pretrained=True, *args, **kwargs):
+    """
+    Pretrained Supervised ChannelViT-Small model (patch size = 8) trained on
+    all channels from WILDS Camelyon17 dataset
+    """
+    model = channelvit_small(patch_size=8, in_chans=18, *args, **kwargs)
+    model.load_state_dict(
+        hub.load_state_dict_from_url(
+            "https://github.com/insitro/ChannelViT/releases/download/v1.0.0/camelyon_channelvit_small_p8_with_hcs_supervised.pth",
+            progress=True
+        )
+    )
+    # Set the model to evaluation mode
+    model.eval()
     return model
